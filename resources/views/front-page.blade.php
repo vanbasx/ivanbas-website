bedca691755499c3309d0662add33b95ee84a355
vanbasx <ivan.bas.vb@gmail.com>
2025-06-02 21:44:04 +0200

    upd hero

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -2,6 +2,50 @@
 
 @section('content')
 
+<section class="bg-zinc-50">
+  <div class="border-zinc-200 border-b h-15"></div>
 
+  <div class="mx-auto px-5 w-full max-w-330">
+    <div class="py-30 border-x border-zinc-200">
+      
+      {{-- /label --}}
+      <div class="flex items-center gap-1 bg-white shadow-xs mx-auto mb-3 px-3 py-1 rounded-full w-fit">
+        <div class="relative bg-green-200 rounded-full size-4 animate-pulse">
+          <div class="top-1/2 left-1/2 absolute bg-green-400 rounded-full size-2 -translate-x-1/2 -translate-y-1/2"></div>
+        </div>
+        <span class="font-medium text-zinc-600 text-xs tracking-wide">
+          Available to work
+        </span>
+      </div>
+      {{-- /label --}}
+
+      {{-- hero title --}}
+      <h1 class="mx-auto mb-3 font-black text-zinc-950 text-7xl/19 text-center uppercase text-balance tracking-tight">
+        High Quality Builds for Ambitious Teams
+      </h1>
+      {{-- /hero title --}}
+
+      {{-- hero subtitle --}}
+      <p class="mx-auto mb-8 text-zinc-600 text-lg/7 text-center tracking-wide">
+        I help founders ship faster, convert better, and raise more
+      </p>
+      {{-- /hero subtitle --}}
+
+      {{-- hero buttons --}}
+      <div class="flex items-center gap-3 mx-auto w-fit">
+        <a href="" class="bg-black px-7 py-3.5 rounded-full font-bold text-white text-sm/5 uppercase tracking-wide">
+          Book an intro call
+        </a>
+        <a href="" class="bg-white px-7 py-3.5 border border-zinc-300 rounded-full font-bold text-zinc-950 text-sm/5 uppercase tracking-wide">
+          Explore my Works
+        </a>
+      </div>
+      {{-- /hero buttons --}}
+
+    </div>
+  </div>
+
+  <div class="border-y border-zinc-200 h-14"></div>
+</section>
 
 @endsection
