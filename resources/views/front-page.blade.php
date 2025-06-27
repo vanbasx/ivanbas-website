68466d8eb8c8bf56a3cd6f4af819abe041af50b5
vanbasx <ivan.bas.vb@gmail.com>
2025-06-27 20:43:44 +0200

    add hero section

---
+++ resources/views/front-page.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+  <section class="mb-25 pt-13 pb-45 border-neutral-700 border-b">
+    <div class="container">
+      <svg class="w-full" id="hero-svg" viewBox="0 0 216 16" xmlns="http://www.w3.org/2000/svg">
+        <foreignObject width="100%" height="100%">
+          <h1 class="block font-bold uppercase text-nowrap leading-none lg:tracking-tight">
+            Ivan Bas <span class="font-light text-neutral-400">©</span> <br class="lg:hidden"> Web Developer
+          </h1>
+        </foreignObject>
+      </svg>
+    </div>
+  </section>
+  <section class="">
+    <div class="container">
+      <h2 class="font-bold text-[40px]/12 text-neutral-50 uppercase tracking-tight">
+        Selected Works
+      </h2> 
+    </div>
+  </section>
+@endsection
