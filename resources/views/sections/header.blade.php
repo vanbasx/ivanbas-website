40d34889930bc8c5d47190cae589706d553cdc99
vanbasx <ivan.bas.vb@gmail.com>
2025-06-30 00:13:21 +0200

    add header

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,5 +1,19 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    
-  </a>
+<header class="top-0 left-0 z-10 fixed bg-zinc-200 border-zinc-400 border-b w-full">
+  <div class="gap-5 grid grid-cols-6 container">
+    <a href="mailto:hello@ivanbas.dev" class="flex items-center gap-1.5 col-span-2 py-5 font-semibold max-sm:text-sm uppercase">
+      <span class="relative flex size-2.5">
+        <span class="inline-flex absolute bg-green-400 opacity-75 rounded-full w-full h-full animate-ping"></span>
+        <span class="inline-flex relative bg-green-500 rounded-full size-2.5"></span>
+      </span>
+      hello@ivanbas.dev
+    </a>
+    <nav class="max-lg:hidden col-span-4">
+      <ul class="gap-5 grid grid-cols-4 font-semibold text-zinc-600 uppercase">
+        <li class="border-zinc-400 border-l"><a href="/" class="block hover:opacity-50 py-5 pl-5 transition-opacity duration-300">Home</a></li>
+        <li class="border-zinc-400 border-l"><a href="" class="block hover:opacity-50 py-5 pl-5 transition-opacity duration-300">Works</a></li>
+        <li class="border-zinc-400 border-l"><a href="" class="block hover:opacity-50 py-5 pl-5 transition-opacity duration-300">Blog</a></li>
+        <li class="border-zinc-400 border-l"><a href="" class="block hover:opacity-50 py-5 pl-5 transition-opacity duration-300">Resume</a></li>
+      </ul>
+    </nav>
+  </div>
 </header>
