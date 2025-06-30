d57c368ef5688fb2ee5af1604d306b3761e16f75
vanbasx <ivan.bas.vb@gmail.com>
2025-06-30 20:17:01 +0200

    add footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,13 @@
-<footer class="content-info">
-  
+<footer class="border-zinc-700 border-t">
+  <div class="flex md:justify-between md:items-center gap-5 container">
+    <div class="py-6 pr-8 max-md:border-zinc-700 max-md:border-r max-sm:text-sm">
+      <span class="text-zinc-400">© 2025</span> All rights reserved
+    </div>
+    <ul class="flex max-md:flex-col items-center max-md:items-start gap-5 max-md:gap-3 max-md:col-span-2 py-6">
+      <li><a target="_blank" href="https://www.linkedin.com/in/ivan-bas" class="inline-block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors duration-300 ease-in-out">LinkedIn</a></li>
+      <li><a target="_blank" href="https://github.com/vanbasx" class="inline-block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors duration-300 ease-in-out">GitHub</a></li>
+      <li><a target="_blank" href="#" class="inline-block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors duration-300 ease-in-out">Codepen</a></li>
+      <li><a target="_blank" href="#" class="inline-block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors duration-300 ease-in-out">Resume</a></li>
+    </ul>
+  </div>
 </footer>
