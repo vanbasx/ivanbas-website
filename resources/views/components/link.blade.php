e41ee716968f929a578dbb4cd18cc8594244b125
vanbasx <ivan.bas.vb@gmail.com>
2025-07-01 23:28:21 +0200

    add targer blank parametr

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,7 +1,12 @@
 @props([
   'href' => null,
+  'blank' => false,
 ])
 
-<a href="{{ $href }}" class="inline-block bg-zinc-900 hover:opacity-50 px-0.5 text-zinc-50 decoration-1 underline transition-opacity duration-300 ease-in-out">
+<a 
+  href="{{ $href }}" 
+  @if($blank) target="_blank" @endif 
+  class="inline-block bg-zinc-900 hover:opacity-50 px-0.5 text-zinc-50 decoration-1 underline transition-opacity duration-300 ease-in-out"
+>
   {!! $slot !!}
 </a>
