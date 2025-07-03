c46079b213863e1f0b5756ecc7e8fb440d9793dc
vanbasx <ivan.bas.vb@gmail.com>
2025-07-03 18:46:08 +0200

    fix <li> size

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -3,11 +3,11 @@
     <div class="py-5 pr-8 max-md:border-zinc-700 max-md:border-r max-sm:text-sm">
       <span class="text-zinc-400">© 2025</span> All rights reserved
     </div>
-    <ul class="flex max-md:flex-col items-center max-md:items-start gap-5 max-md:gap-3 max-md:col-span-2 py-5">
-      <li><a target="_blank" href="https://www.linkedin.com/in/ivan-bas" class="inline-block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors">LinkedIn</a></li>
-      <li><a target="_blank" href="https://github.com/vanbasx" class="inline-block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors">GitHub</a></li>
-      <li><a target="_blank" href="#" class="inline-block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors">Codepen</a></li>
-      <li><a target="_blank" href="#" class="inline-block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors">Resume</a></li>
+    <ul class="flex max-md:flex-col items-center max-md:items-start gap-5 max-md:gap-4 max-md:col-span-2 py-5">
+      <li><a target="_blank" href="https://www.linkedin.com/in/ivan-bas" class="block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors">LinkedIn</a></li>
+      <li><a target="_blank" href="https://github.com/vanbasx" class="block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors">GitHub</a></li>
+      <li><a target="_blank" href="#" class="block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors">Codepen</a></li>
+      <li><a target="_blank" href="#" class="block text-zinc-400 hover:text-zinc-50 max-sm:text-sm transition-colors">Resume</a></li>
     </ul>
   </div>
 </footer>
