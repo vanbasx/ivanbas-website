d6c50ee90563fe6b2321aea707b93e4ce87cbab5
vanbasx <ivan.bas.vb@gmail.com>
2025-07-05 21:30:47 +0200

    add controller for front page

---
+++ app/View/Composers/FrontPage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Composers;
+
+use Roots\Acorn\View\Composer;
+use WP_Query;
+
+class FrontPage extends Composer
+{
+    protected static $views = [
+      'front-page',
+    ];
+
+    public function with(): array
+    {
+      return [
+        'works' => $this->getWorks(),
+        // 'blog' => $this->getBlog(),
+      ];
+    }
+
+    protected function getWorks(): WP_Query
+    {
+      return new WP_Query([
+        'post_type' => 'works',
+        'posts_per_page' => 2,
+      ]);
+    }
+}
